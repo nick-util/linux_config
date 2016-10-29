2f8b3a1b90478a1cd3fcf38fbe76126e1249d43d
Your Name <you@example.com>
2016-10-29 13:57:15 -0700

    upd

---
+++ basic_utilities.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+
+# basic utilities
+sudo apt-get install unzip
+
---
+++ docker.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+sudo apt-get update
+
+{
+  sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+  echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" | sudo tee /etc/apt/sources.list.d/docker.list
+  sudo apt-get update
+  sudo apt-get install -y docker-engine
+
+} || {
+
+    echo "Docker Install failed"
+    return 0;
+}
+
+{ # try
+
+  curl -L https://github.com/docker/compose/releases/download/1.9.0-rc2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose &&
+  chmod +x /usr/local/bin/docker-compose
+
+} || { # catch
+  echo "Docker Compose install failed"
+}
