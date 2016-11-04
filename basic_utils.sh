992ee3cfc00c972e82637dd7916bc7196bd37d39
Nick Kiermaier <nkiermaier@users.noreply.github.com>
2016-11-04 13:43:56 -0700

    Update basic_utils.sh

--- basic_utils.sh
+++ basic_utils.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-utils=(unzip p7zip-full git curl htop wget ccrypt)
+utils=(unzip p7zip-full git curl htop wget ccrypt build-essential libxml2 libxml2-dev 
+libcurl4-openssl-dev libmcrypt-dev libreadline-dev)
 
 
 
