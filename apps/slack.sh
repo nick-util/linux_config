469bfc82c094835bdb26267d681df8eed16d60b7
Nick Kiermaier <nkiermaier@users.noreply.github.com>
2016-11-11 10:34:13 -0800

    Update slack.sh

--- apps/slack.sh
+++ apps/slack.sh
@@ -1,4 +1,5 @@
 # https://www.howtoinstall.co/en/ubuntu/trusty/slack
 # https://slack.com/downloads/linux
+# https://installion.co.uk/ubuntu/precise/universe/s/slack/install/index.html
 
 sudo apt-get install slack
