cb7120df947d8da97debca308bb46ed18d606c8a
Nick Kiermaier <nkiermaier@users.noreply.github.com>
2016-11-23 13:48:48 -0800

    Update insync.sh

--- apps/insync.sh
+++ apps/insync.sh
@@ -1,3 +1,11 @@
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys ACCAF35C
-sudo apt-get update
-sudo apt-get install insync
+# Google drive client for linux
+# the apt-get package install method doesnt seem to work
+
+# sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys ACCAF35C
+# sudo apt-get update
+# sudo apt-get install insync
+
+# other istall options
+# https://www.insynchq.com/downloads
+
+
