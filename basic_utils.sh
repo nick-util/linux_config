6ddc82336aa7b60e42a22ff5337d4acb5c8fe4c7
Nick Kiermaier <nkiermaier@users.noreply.github.com>
2016-11-23 12:56:12 -0800

    Update basic_utils.sh

--- basic_utils.sh
+++ basic_utils.sh
@@ -19,3 +19,5 @@ done
   git config --global user.email "NickKiermaier@gmail.com"
   git config --global user.name "NickKiermaier"
 
+# rbenv-build suggested build environment
+sudo apt-get install autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm3 libgdbm-dev
