97d01aca0f2b631c8840896034f7b597cd3be175
Nick Kiermaier <nkiermaier@users.noreply.github.com>
2016-11-23 12:38:03 -0800

    Update ruby.sh

--- ruby.sh
+++ ruby.sh
@@ -2,9 +2,10 @@
 
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv
 cd ~/.rbenv && src/configure && make -C src
-echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
 ~/.rbenv/bin/rbenv init
-echo "eval "$(rbenv init -)" >> ~/.bash_profile
+echo "eval "$(rbenv init -)" >> ~/.bashrc
+exec $SHELL
 
 # ruby-build addon
 # https://github.com/rbenv/ruby-build#readme
