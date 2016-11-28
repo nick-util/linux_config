f00da42bf292547f1f35da7dc3d620b50551a4e5
Nick Kiermaier <nkiermaier@users.noreply.github.com>
2016-11-28 06:44:20 -0800

    Update basic_utils.sh

--- basic_utils.sh
+++ basic_utils.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 utils=(unzip p7zip-full git curl htop wget ccrypt build-essential libxml2 libxml2-dev 
 libcurl4-openssl-dev libmcrypt-dev libreadline-dev libmysqlclient-dev
- libffi-dev libpq-dev lm-sensors hddtemp psensor unity-tweak-tool python-pip nodejs)
+ libffi-dev libpq-dev lm-sensors hddtemp psensor unity-tweak-tool python-pip nodejs flashplugin-installer)
 
 
 
